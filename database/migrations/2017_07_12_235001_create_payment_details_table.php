bb49d8238fbe69708ba155550f7a7e755aea3f27
Huon Imberger <s3527782@student.rmit.edu.au>
2017-07-13 10:17:32 +1000

    Add models and migrations

---
+++ database/migrations/2017_07_12_235001_create_payment_details_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePaymentDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payment_details', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('cc_name');
+            $table->string('cc_number');
+            $table->integer('cc_expiry_month');
+            $table->integer('cc_expiry_year');
+            $table->integer('cc_cvc');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payment_details');
+    }
+}
---
+++ database/migrations/2017_07_12_235017_create_wishlist_items_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateWishlistItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('wishlist_items', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('comment');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('wishlist_items');
+    }
+}
