392c8fa52bea69c2eafe2835a47b083b1b69116a
Huon Imberger <s3527782@student.rmit.edu.au>
2017-07-14 15:50:14 +1000

    Dynamically build carousel from database
    Shows featured movies based on Movie->featured

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Movie;
 
 class HomeController extends Controller
 {
@@ -23,6 +24,6 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        return view('home')->with('featured_movies', Movie::where('featured', true)->get());
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,39 +1,40 @@
 @extends('layouts.app')
 
 @section('content')
-<div id="featured-movie-carousel" class="carousel slide" data-ride="carousel">
-    <!-- Indicators -->
-    <ol class="carousel-indicators">
-        <li data-target="#featured-movie-carousel" data-slide-to="0" class="active"></li>
-        <li data-target="#featured-movie-carousel" data-slide-to="1"></li>
-        <li data-target="#featured-movie-carousel" data-slide-to="2"></li>
-    </ol>
+    <div class="row">
+        <div class="col-md-12">
+            <div id="featured-movie-carousel" class="carousel slide" data-ride="carousel">
+                <!-- Indicators -->
+                <ol class="carousel-indicators">
+                    @foreach($featured_movies as $movie)
+                        <li data-target="#featured-movie-carousel" data-slide-to="{{$loop->index}}" {!! $loop->first ? 'class="active"' : '' !!}></li>
+                    @endforeach
+                </ol>
 
-    <!-- Wrapper for slides -->
-    <div class="carousel-inner">
-        <div class="item active">
-            <img src="media/movie_art/1_banner.jpg" alt="Los Angeles">
-        </div>
+                <!-- Wrapper for slides -->
+                <div class="carousel-inner">
+                    @foreach($featured_movies as $movie)
+                        <div class="item {!! $loop->first ? 'active' : '' !!}">
+                            <a href="#">
+                                <img src="media/movie_art/{{$movie->id}}_banner.jpg">
+                            </a>
+                        </div>
+                    @endforeach
+                </div>
 
-        <div class="item">
-            <img src="media/movie_art/2_banner.jpg" alt="Chicago">
-        </div>
-
-        <div class="item">
-            <img src="media/movie_art/3_banner.jpg" alt="New York">
+                <!-- Left and right controls -->
+                <a class="left carousel-control" href="#featured-movie-carousel" data-slide="prev">
+                    <span class="glyphicon glyphicon-chevron-left"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="right carousel-control" href="#featured-movie-carousel" data-slide="next">
+                    <span class="glyphicon glyphicon-chevron-right"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+            </div>
         </div>
     </div>
 
-    <!-- Left and right controls -->
-    <a class="left carousel-control" href="#featured-movie-carousel" data-slide="prev">
-        <span class="glyphicon glyphicon-chevron-left"></span>
-        <span class="sr-only">Previous</span>
-    </a>
-    <a class="right carousel-control" href="#featured-movie-carousel" data-slide="next">
-        <span class="glyphicon glyphicon-chevron-right"></span>
-        <span class="sr-only">Next</span>
-    </a>
-</div>
 
-    <h1>blah</h1>
+    <h3>Take the family to the cinema today!</h3>
 @endsection
