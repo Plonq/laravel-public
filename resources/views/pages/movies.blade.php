861a942e105e9f81dd259e185069e150cf408038
Huon Imberger <huon28@gmail.com>
2017-07-26 09:31:23 +1000

    Add sessions/info button to coming soon movies

--- resources/views/pages/movies.blade.php
+++ resources/views/pages/movies.blade.php
@@ -45,6 +45,7 @@
                             <th>Genre</th>
                             <th>Rating</th>
                             <th>Release Date</th>
+                            <th>Sessions/Info</th>
                         </tr>
                         </thead>
                         <tbody>
@@ -54,6 +55,7 @@
                                 <td>{{$movie->genre->name}}</td>
                                 <td>{{$movie->rating->name}} ({{$movie->rating->code}})</td>
                                 <td>{{$movie->release_date_string}}</td>
+                                <td><a class="btn btn-sm btn-default" role='button' href="{{route('movie', $movie->id)}}">Sessions/Info</a></td>
                             </tr>
                         @endforeach
                         </tbody>
